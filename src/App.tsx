c7cd793ac0f7649f3eb02eb66914d19ba781dc2b
isaiasdesemvolvedor <isaiasdesemvolvedor>
2023-12-23 15:13:40 -0300

    climatico

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import { useState } from 'react';
+
+
+
+
+function App() {
+
+  return (
+<body> 
+<div className="container">
+  <h1>Formulário de Contato</h1>
+  <form id="contact-form" method="POST" action="https://formsubmit.co/isaiasnovaera@gmail.com">
+    <label htmlFor="name">Nome:</label>
+    <input type="text" id="name" name="name" required />
+    <label htmlFor="idade">Idade:</label>
+    <input type="number" id="idade" name="idade" required />
+    <label htmlFor="email">Gmail</label>
+    <input type="email" id="email"/> 
+    <label htmlFor="message">Mensagem:</label>
+    <input type="hidden" name="_next" value="http://localhost:3000" />
+    <textarea  id="message"
+      name="message"
+      rows={4}
+      placeholder="Esse formulário será enviado para o email:isaiasnovara@gmail.com"/>
+    <button type="submit" >Enviar</button>
+  </form>
+</div>
+</body>
+ )
+ }
+
+
+export default App;
